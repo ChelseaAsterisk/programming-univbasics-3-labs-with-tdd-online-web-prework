621984d78e7f102535fc0a14b76755e4224436c5
Chelsea Lopez <chelseaalopez@yahoo.com>
2020-01-22 18:45:17 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
 first_number = Float
-second_number = Float
+second_number = Float !=0
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
